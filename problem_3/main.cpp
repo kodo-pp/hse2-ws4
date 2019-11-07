1aa1449d0532463a180c6684912aac7c96f3bbfd
Alexander Korzun <korzun.sas@mail.ru>
2019-11-07 09:32:33 +0300

    Solve the third task

--- problem_3/main.cpp
+++ problem_3/main.cpp
@@ -2,14 +2,27 @@
 
 #include <iostream>
 
-int custom_strlen (const char string[])
+
+size_t custom_strlen(const char* string)
 {
-  /// Input your code here
-  return 0;
+    size_t length = 0;
+    while (*string != '\0') {
+        ++string;
+        ++length;
+    }
+    return length;
 }
+
 
 int main()
 {
-    std::cout << custom_strlen ("Hello World") << std::endl;
+    std::cout << custom_strlen("Hello World") << std::endl;
+
+    /*
+    std::string s;
+    std::cin >> s;
+    std::cout << custom_strlen(s.c_str()) << std::endl;
+    */
+
     return 0;
 }
